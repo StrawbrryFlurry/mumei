cee563b924a1d0df0f59f817c2082a66b8ca5e3e
Michael <michi.straub@icloud.com>
2022-08-28 05:39:26 +0200

    🔨 [CodeGen] Syntax.SetParent now also sets TypeContext

--- src/Mumei.CodeGen/SyntaxNodes/Syntax.cs
+++ src/Mumei.CodeGen/SyntaxNodes/Syntax.cs
@@ -41,5 +41,6 @@ public abstract class Syntax {
     }
 
     Parent = parent;
+    TypeContext = parent.TypeContext;
   }
 }
--- test/Mumei.CodeGen.Test/SyntaxNodes/SyntaxTests.cs
+++ test/Mumei.CodeGen.Test/SyntaxNodes/SyntaxTests.cs
@@ -23,8 +23,20 @@ public class SyntaxTests {
     action.Should().Throw<InvalidOperationException>();
   }
 
+  [Fact]
+  public void SetParent_SetsTypeContextOfNode_ToContextOfParent() {
+    var newParent = new SyntaxImpl();
+    var sut = new SyntaxImpl();
+
+    sut.SetParent(newParent);
+
+    sut.PublicTypeContext.Should().Be(newParent.PublicTypeContext);
+  }
+
   private class SyntaxImpl : Syntax {
     public SyntaxImpl(Syntax parent = null) : base(parent) { }
+
+    public SyntaxTypeContext PublicTypeContext => TypeContext;
 
     public override void WriteAsSyntax(ITypeAwareSyntaxWriter writer) {
       throw new NotImplementedException();
