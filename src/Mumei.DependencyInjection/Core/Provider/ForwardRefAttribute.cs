2d9c0206766c472f86388d8b96fd786811fbe54c
Michael <michi.straub@icloud.com>
2023-06-23 00:11:14 +0200

    ✏️ [DI] Change ForwardRef usage

--- src/Mumei.DependencyInjection/Core/Provider/ForwardRefAttribute.cs
+++ src/Mumei.DependencyInjection/Core/Provider/ForwardRefAttribute.cs
@@ -2,23 +2,25 @@
 
 public enum ForwardRefSource {
   /// <summary>
-  /// Requires the provider to be declared in a parent component or module.
+  /// Declares that the provider can be resolved through the parent module.
+  /// Because the parent module is always able to resolve all providers in it's children,
+  /// this can also be used for declaring providers that are part of sibling component implementations in different
+  /// modules.
   /// </summary>
   Parent,
 
   /// <summary>
-  /// Requires the provider to be declared in a sibling of a subcomponent.
+  /// Declares that the provider is available through the parent injector, same as <see cref="Parent"/>,
+  /// but disables static analysis for the provider when building the dependency graph.
   /// </summary>
-  Sibling
+  Dynamic
 }
 
 [AttributeUsage(AttributeTargets.Property)]
 public sealed class ForwardRefAttribute : Attribute {
   public ForwardRefSource Source { get; }
 
-  public ForwardRefAttribute() { }
-
-  public ForwardRefAttribute(ForwardRefSource source) {
+  public ForwardRefAttribute(ForwardRefSource source = ForwardRefSource.Parent) {
     Source = source;
   }
 }
