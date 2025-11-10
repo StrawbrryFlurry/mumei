085d212597009fd02271afd90f6690da0a8011c9
Michael <michi.straub@icloud.com>
2025-11-10 01:01:46 +0100

    I love my semantic model!

--- src/Mumei.CodeGen.Qt/CodeGen/SyntheticClass/SyntheticClassMethodDeclarationGenerator.cs
+++ src/Mumei.CodeGen.Qt/CodeGen/SyntheticClass/SyntheticClassMethodDeclarationGenerator.cs
@@ -1,4 +1,57 @@
-﻿namespace Mumei.CodeGen.Qt;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+using Mumei.CodeGen.Qt.TwoStageBuilders.RoslynCodeProviders;
+
+namespace Mumei.CodeGen.Qt;
 
 // Responsible for generating the interceptors for synthetic class methods e.g. WithBody()
-internal sealed class SyntheticClassMethodDeclarationGenerator { }
+internal sealed class SyntheticClassMethodDeclarationGenerator : IIncrementalGenerator {
+    public void Initialize(IncrementalGeneratorInitializationContext context) {
+        var codeFragmentInvocations = context.SyntaxProvider.CreateSyntaxProvider(
+            IsCodeFragmentInvocation,
+            TranslateCodeFragmentInvocationProvider
+        ).Where(x => x);
+
+        // var interceptorMethods = codeFragmentInvocations.Select(CreateCodeFragmentInterceptorMethod);
+//
+        context.RegisterSourceOutput(codeFragmentInvocations, (ctx, state) => { });
+    }
+
+    private static bool IsCodeFragmentInvocation(SyntaxNode node, CancellationToken _) {
+        return SyntaxNodeFilter.IsInvocationOf(node, "CallSomethingWithTypeSymbol");
+    }
+
+    private static InterceptInvocationIntermediateNode<LambdaExpressionSyntax> TranslateCodeFragmentInvocationProvider(GeneratorSyntaxContext context, CancellationToken _) {
+        if (!SyntaxProviderFactory.TryCreateInvocation(context, out var invocation)) {
+            return IntermediateNode.None;
+        }
+
+        var arg = invocation.Invocation.ArgumentList.Arguments[0].Expression;
+        var syntheticMacroInvocation = SyntaxFactory.InvocationExpression(
+            SyntaxFactory.MemberAccessExpression(
+                SyntaxKind.SimpleMemberAccessExpression,
+                arg,
+                SyntaxFactory.IdentifierName("Bind__self")
+            )
+        );
+
+        var macroInvocation = context.SemanticModel.GetSpeculativeSymbolInfo(arg.SpanStart, syntheticMacroInvocation, SpeculativeBindingOption.BindAsExpression);
+        if (macroInvocation.Symbol is not IMethodSymbol macroBindingMethod) {
+            return IntermediateNode.None;
+        }
+
+        var __ = macroBindingMethod;
+
+        if (!invocation.MethodInfo.IsDeclaredIn<CodeFragment>()) {
+            return IntermediateNode.None;
+        }
+
+        if (invocation.Operation.Arguments is not [{ Value: IDelegateCreationOperation { Syntax: LambdaExpressionSyntax fragmentDeclaration } }]) {
+            return IntermediateNode.None;
+        }
+
+        return invocation.AsIntercept().WithState(fragmentDeclaration);
+    }
+}
---
+++ test/Mumei.CodeGen.Qt.Tests/Testing/SyntheticClass/SyntheticClassMethodDeclarationGeneratorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+using Mumei.CodeGen.Qt.Tests.Setup;
+using SourceCodeFactory;
+
+namespace Mumei.CodeGen.Qt.Tests.Testing;
+
+public sealed class SyntheticClassMethodDeclarationGeneratorTests {
+    [Fact]
+    public void Test() {
+        var result = new SourceGeneratorTest<SyntheticClassMethodDeclarationGenerator>(b =>
+            b.AddReference(SourceCode.Of<CompilationTestSource>()).WithAssemblyName("TestAssembly")
+        ).Run();
+    }
+}
+
+file sealed class CompilationTestSource {
+    // Include into the compilation
+    private Delegate _ref = TypeExtensions.Bind__self;
+
+    public void TestInvocation() {
+        var typeSymbol = default(ITypeSymbol)!;
+        CallSomethingWithTypeSymbol(typeSymbol);
+    }
+
+    public void CallSomethingWithTypeSymbol(ITypeSymbol typeSymbol) { }
+}
+
+public static class TypeExtensions {
+    extension(ITypeSymbol typeSymbol) {
+        public string Bind__self() {
+            return "Haii! :)";
+        }
+    }
+}
